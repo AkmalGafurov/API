0270a999ddb162108a7f3c3132bb81aebc59fed5
AkmalGafurov <akmalgofurov05@gmail.com>
2026-01-07 17:11:14 +0500

    CRUD

---
+++ app/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views_fbv import car_list_create, car_detail
+from .views_cbv import (
+    CarListCreateAPIView,
+    CarRetrieveUpdateDestroyAPIView
+)
+
+urlpatterns = [
+    path('cars/', car_list_create),
+    path('cars/<int:pk>/', car_detail),
+    path('cbv/cars/', CarListCreateAPIView.as_view()),
+    path('cbv/cars/<int:pk>/', CarRetrieveUpdateDestroyAPIView.as_view()),
+]
+
+
+
